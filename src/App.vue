dc88d25122324c94445ffc79b545a9140092da72
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-21 11:31:23 +0000

    added image and header to home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
 	<the-header></the-header>
-	<div>
+	<div class="main">
+		<h1 class="header">Boyle Celtic FC</h1>
+	</div>
+	<div class="home">
 		<home></home>
 	</div>
 	
@@ -24,6 +27,33 @@ export default {
 </script>
 
 <style>
+	.main{
+		position: fixed;
+		width: 100%;
+		background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url('./images/boylecelticpitch.jpeg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+		height: 500px;
+	}
+	.header{
+		margin-top: 250px;
+		text-align: center;
+		font-size: 4rem;
+		color: #fff;
+	}
+	.home{
+		width: 99%;
+		border: solid 1px rgb(238, 238, 238);
+		border-radius: 3px;
+		z-index: 100000;
+		top: 50%;
+		left: 50%;
+		position: absolute;
+		transform: translate(-50%, 60%);
+		background-color: #fff;
+		height: 1100px;	
+	}
 
 	.route-enter-from{
 		opacity: 0;
